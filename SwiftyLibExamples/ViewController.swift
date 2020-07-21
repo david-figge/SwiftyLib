8025f383697f97bb93589f5e2abefefddbb6136f
David Figge <david.figge@bestbuy.com>
2020-07-21 14:07:22 -0700

    Initial project setup

---
+++ SwiftyLibExamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftyLibExamples
+//
+//  Created by Figge, David-CW on 7/21/20.
+//  Copyright © 2020 Figge, David-CW. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
