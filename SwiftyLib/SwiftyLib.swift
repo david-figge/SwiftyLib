35da4c6def1c4b1d477e52223ba900f20abb93d2
David Figge <david.figge@bestbuy.com>
2020-08-25 15:16:47 -0700

    Made functions static

--- SwiftyLib/SwiftyLib.swift
+++ SwiftyLib/SwiftyLib.swift
@@ -6,11 +6,11 @@ public final class SwiftyLib {
     
     let name = "SwiftyLib"
     
-    public func add(a: Int, b: Int) -> Int {
+    public static func add(a: Int, b: Int) -> Int {
         return a + b;
     }
     
-    public func sub(a: Int, b: Int) -> Int {
+    public static func sub(a: Int, b: Int) -> Int {
         return a - b;
     }
     
